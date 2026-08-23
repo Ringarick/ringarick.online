ce7bc6219521abfba5e838e9170385c59a3a52c5
Ringarick <potapovyra000@gmail.com>
2026-08-23 13:18:57 +0300

    added switch at js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const themeButton = document.querySelector("#theme-button");
 const body = document.body;
+
+themeButton.addEventListener("click", () => {
+    body.classList.toggle("light-theme");
+    body.classList.toggle("dark-theme");
+}
